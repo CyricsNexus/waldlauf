32a335fb8f4376a2d3be7a6980d38cd56dac54ea
CFRA204 <CFRA204@PC42192191.cc-college.gecs.loc>
2025-06-17 14:05:32 +0200

    neues Package erstellt

---
+++ Waldlauf_Git/src/prozedural/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * das prozedurale package ist die Erweiterung zu "strukturiert_mit_err".
+ * hier wird gezeigt, wie durch Prozeduren eine bessere Übersichtlichkeit kommen kann
+ */
+/**
+ * @version 1.0
+ * @author C. Frantzen
+ * @since 1.0
+ **/
+package prozedural;
