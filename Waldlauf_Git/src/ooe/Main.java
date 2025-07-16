ed407bce86f5f180e85e71a8b32525783b3637e3
CFRA204 <CFRA204@PC42192191.cc-college.gecs.loc>
2025-07-16 08:39:14 +0200

    OOE-Ansatz hinzugefügt

---
+++ Waldlauf_Git/src/ooe/Main.java
@@ -0,0 +1,156 @@
+package ooe;
+
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+	/*********************************************************************************
+	 *  Ein Unterschied zu dem prozeduralen Beispiel ist, dass ich die Main-Methode nicht ganz so entschlackt habe.
+	 *  	Während ich im Prozeduralen Programmierp. zeigen wollte, wie weit man Prozeduren verschachteln kann, ist mir hier
+	 *  die generelle Lesbarkeit wichtiger. Hier wurden nur Prozeduren in der Main-Methode erstellt, 
+	 *  die Fehler abfangen sollen oder die Daten bereitstellen. 
+	 *  
+	 *  Viele Methoden sind hier nicht mehr aufgeführt, weil diese Teil des Spiels (Klasse Spiel) sind und nicht Teil des Ablaufs
+	 *  
+	 *  Man hätte hier noch Sicherheitsmaßnahmen zulasten des Speichers und der Verarbeitungszeit einbauen können, wie z.B. 
+	 *  ein Switch-Case nach der Entscheidung, dass die Benutzerauswahl einen String "streicheln", "fuettern" oder "hauen" übergibt 
+	 *  der dann von der Klasse ListeBegegnung ausgewertet wird, statt die direkte Eingabe 1, 2 oder 3, um Programmierfehler zu vermeiden.
+	 *  Hier gibt es kein konkretes "richtig" oder "falsch". Da dieses in einem Zug verarbeitet werden konnte und nicht davon
+	 *  auszugehen ist, dass dieses zukünftig erweitert werden soll, wurde auf die Sicherheitsmaßnahme zugunsten Ressourcen verzichtet
+	 * ******************************************************************************/
+	
+	public static void main(String[] args) throws IOException {
+		//----------------------------------------
+		// INIT
+		
+		Scanner sc = new Scanner(System.in);							// Instanziierung Benutzereingabe
+		Random random = new Random();								// Instanziierung Zufallszahl für Zufallsbegegnungen
+		
+		Spiel meinSpiel = new Spiel(3, 10); 							// Spiel mit 3 Leben und 10 Runden
+		ListeBegegnung listeBegegnung = addBegegnungenImSpiel();		// diese ist dynamisch und benötigt daher keine vorherige Angabe, wie viele Begegnungen vorhanden sind
+		
+		//----------------------------------------		
+	    // SPIELABLAUF
+		
+		Spiel.ausgebenIntro();										// Spielintro
+		
+		// Schleife, welche so lange durchlaufen wird, bis kein Leben vorhanden ist oder die letzte Runde geschafft wurde  
+		for(int runde = 1; runde <= meinSpiel.getMaxRunde(); runde++) {
+			int zufallsBegegnung = random.nextInt(listeBegegnung.getListeBegegnung().size());				// Zufällige Auswahl der Begegnung
+			if (runde > 1) Spiel.ausgebenFolgebegegnung();												// Rundenanfangstext, wenn Intro aus Spielbeginn nicht angezeigt wurde
+			byte entscheidung = eingabeBenutzer(sc);														// Benutzerauswahl + Überprüfung der gültigen Benutzereingabe 1 (streicheln), 2 (füttern) oder 3 (hauen)
+			Spiel.leerenKonsole();																		// Bildschirm vor Auswirkung leeren, damit bessere Lesbarkeit in der Konsole vorhanden ist
+			listeBegegnung.ausgabeReaktion(zufallsBegegnung, entscheidung); 								// Ausgabe der Reaktion der Begegnung auf Benutzereingabe
+			if(listeBegegnung.isAbzugLeben(zufallsBegegnung, entscheidung)) meinSpiel.lebenVerringern();	// Wenn Benutzerauswahl schlecht war, dann Leben verringern
+			
+			if(!meinSpiel.lebenVorhanden()) break;														// Vorzeitiger Abbruch, falls kein Leben mehr vorhanden
+		}
+		
+		meinSpiel.ausgebenNachrichtEnde();																// Ausgabe, ob man gewonnen oder verloren hat
+		
+		//----------------------------------------		
+	    // AUFRÄUMEN - HEAP LEEREN
+		random = null;
+		sc.close();
+		sc = null;
+		meinSpiel = null;
+		listeBegegnung = null;
+	}
+	
+	/*********************************************************************************	
+	* Um die Main-Methode nicht unnötig aufzublähen, wurden die Begegnungen in diese Methode
+	* extrahiert. Zurückgegeben wird eine Liste mit Begegnungen
+	* 
+	* Durch die Vorgabe der Parameter und Unterstützung der IDE, ist eine fehlerhafte Eingabe hier wesentlich schwieriger.
+	* Auch ist es bereits bei der Eingabe nicht mehr möglich, falsche Datentypen beim Lebensabzug einzugeben. 
+	* Die Fehlermeldung würde direkt hier aufploppen, wo die Ursache ist und nicht später bei der Verarbeitung.
+	*********************************************************************************/
+	
+	public static ListeBegegnung addBegegnungenImSpiel() {
+		ListeBegegnung listeBegegnung = new ListeBegegnung();
+		
+		listeBegegnung.addBegegnung("Hulk", 
+				"Der Hulk erwiedert deine Zärtlichkeit und drückt dich mal ganz dolle - ohoh! (-1 Leben).", true, // reaktionSteicheln + Lebensabzug
+				"Der Hulk freut sich über das Essen und zieht weiter.", false, // reaktionFuettern + Lebensabzug					
+				"HULK SMASH!(-1 Leben).", true	// reaktionHauen + Lebensabzug
+				);
+
+		listeBegegnung.addBegegnung("Dr. Snuggles", 
+				"Dr. Snuggles drückt Dir zum Abschied freundlich die Hand und hüpft auf seinem Pogo-Schirm weiter.", false, // reaktionSteicheln + Lebensabzug
+				"Dr. Snuggles bedankt sich höflich für das Essen und hüpft auf seinem Pogo-Schirm weiter.", false, // reaktionFuettern + Lebensabzug	
+				"Na, das ist aber nicht nett, Dr. Snuggles zu verhauen. Die Tiere des Waldes sind erbost und \nvermoppern dich dafür gründlich. (-1 Leben).", true	// reaktionHauen + Lebensabzug
+				);
+
+		listeBegegnung.addBegegnung("Batman", 
+				"Batman nickt dir aufmunternd zu. \"Freundlichkeit gegenüber Unbekannten kann nie schaden. Wie sagte \nnoch einst ein weiser Mann aus dem gebildeten Orient...\" Du wachst nach einiger Zeit benommen auf und \nsetzt Deinen Weg fort.", false, // reaktionSteicheln + Lebensabzug
+				"Batman freut sich sehr für das Essen. \"Ein gutgenährter Magen  ist die Grundvoraussetzung für einen \ngut genährten Geist. Vergiss das niemals, Robin!\" Bevor du etwas sagen kannst, ist er schon entfleucht.", false, // reaktionFuettern + Lebensabzug
+				"Oh nein! Du hast Batman gehauen. Heiliger Konterschlag! (-1 Leben)", true	// reaktionHauen + Lebensabzug
+				);
+		
+		listeBegegnung.addBegegnung("Pennywise", 
+				"\"Komm mit! Mit uns kannst Du fliiiieeegeeeen!\" Der gruselige Clown winkt und schon bist Du im Gulli (-1 Leben)", true, // reaktionSteicheln + Lebensabzug
+				"\"Magst Du auch Popcorn? Dann komm mit! Bei uns gibt es ganz viel davon!\" Der gruselige Clown gibt \nDir einen roten Ballon und zieht dich in den Gulli. (-1 Leben)", true, // reaktionFuettern + Lebensabzug 	
+				"Du knüppelst auf Pennywise drauf. Du magst eh keine Ballons.", false	// reaktionHauen + Lebensabzug
+				);
+		
+		listeBegegnung.addBegegnung("T-Rex", 
+				"Der T-Rex mag es, gestreichelt zu werden. Dann verschlingt er Dich. (-1 Leben)", true, 	// reaktionSteicheln + Lebensabzug
+				"Der T-Rex guckt hungrig. Du bist das Essen. (-1 Leben)", true, // reaktionFuettern + Lebensabzug
+				"Der T-Rex rennt weinend weg.", false	// reaktionHauen + Lebensabzug
+				);
+		
+		listeBegegnung.addBegegnung("Baum", 
+				"Der Baum streichelt nicht zurück.", false, // reaktionSteicheln + Lebensabzug
+				"Der Baum ist unbeeindruckt vom Essen.", false, 	// reaktionFuettern + Lebensabzug
+				"Der Baum kippt um. Bumm. (-1 Leben)", true	// reaktionHauen + Lebensabzug
+				);
+		
+		listeBegegnung.addBegegnung("Lama", 
+				"Das Lama macht \"Bääääh?\"", false, // reaktionSteicheln + Lebensabzug
+				"Das Lama macht \"Bääää!\" *Mampf*", false, 	// reaktionFuettern + Lebensabzug
+				"Das Lama spuckt. (-1 Leben)", true	// reaktionHauen + Lebensabzug
+				);
+		
+		/* VORLAGE für weitere Begegnungen	
+		listeBegegnung.addBegegnung("", 
+				"", false, 	// reaktionSteicheln + Lebensabzug
+				"", false, // reaktionFuettern + Lebensabzug
+				"", false	// reaktionHauen + Lebensabzug
+				);
+		 */		
+		
+		return listeBegegnung;
+	}
+	
+	/*********************************************************************************	
+	* Diese Methode habe ich nicht in die Klasse Spiel verschoben, da es lediglich um die Überprüfung einer 
+	* korrekten Eingabe (1, 2 oder 3) geht und das keine spezielle Mechanik des Spiel-Objekts ist, sonder den 
+	* korrekten Ablauf in der Main-Methode sichert.
+	* *******************************************************************************/
+	
+	private static byte eingabeBenutzer(Scanner sc) throws IOException {
+
+		byte eingabe = 0;
+		boolean eingabeGueltig = false;									// für Errorhandling, dass Benutzer 1, 2 oder 3 eingibt
+
+		// Eingabe des Benutzers + Errorhandling
+		while(!eingabeGueltig) {					// Solange keine gültige Eingabe vorhanden ist, soll die Eingabe wiederholt werden
+			try {									// Überprüfe, ob Fehler gemacht wurde
+				eingabe = sc.nextByte();		// BenutzerEingabe 1, 2 oder 3
+				if (eingabe >=1 && eingabe <=3) {	// Falls Eingabe=Byte, dann überprüfen, ob Wert zwischen 1-3
+					eingabeGueltig = true;
+				} else {							// 		Wenn Wert nicht zwischen 1-3 Fehlermeldung und Wiederholung
+					Spiel.ausgebenNachrichtEingabefehler();
+				}					
+			} catch (InputMismatchException e) {	// Wenn Eingabe kein Byte-Datentyp
+				Spiel.ausgebenNachrichtEingabefehler();
+				sc.next();
+			}
+		}
+		return eingabe;
+	}
+
+}
---
+++ Waldlauf_Git/src/ooe/Spiel.java
@@ -0,0 +1,83 @@
+package ooe;
+
+/* Das Spiel selbst fungiert hier ebenfalls als Objekt mit eigenen Eigenschaften.
+ * Hätte man das nicht weiterhin in der Main-Klasse als Eigenschaften lassen können? Prinzipiell ja.
+ * Es ist einfach sauberer in der Objektorientierten Welt, in der wir alles als Objekte definieren. Und es wird
+ * lesbarer, da wir immer angeben müssen, mit welchem Objekt etwas gemacht wird und welche Objekte sich
+ * gegenseitig bedingen.
+ * 
+ * Die Main-Methode soll streng genommen nur das Spiel starten. In unserem Fall belassen wir in der Main-Methode aber
+ * auch die Algorithmik, da das Spiel so simpel ist - es gibt quasi nichts anderes als den einen Ablauf.
+ * Nach meinem persönlichen Ermessen würde es sich anfühlen wie eine Schachtel zu kaufen, um eine kleinere Schachtel
+ * reinzutun. Es wäre etwas anderes, wenn dieses Spiel eines von mehreren wäre, welches man im Waldlauf machen kann
+ * (z.B. rasten, trainieren, etc.). In diesem Fall hätte ich definitv die Klasse main als reine Startklasse genommen
+ * und ggf. die anderen Parts sogar in eigene Packages sortiert, um diese modular hinzufügen oder entfernen zu können.
+ */
+
+public class Spiel {
+	
+	private int leben = 0;							// Anzahl der Leben
+	private int maxRunde = 0;						// zu durchlaufende Runden
+	
+	public Spiel(int leben, int maxRunde) {
+		this.leben = leben;
+		this.maxRunde = maxRunde;
+	}
+	
+	// Benutzer hat falsche Entscheidung getroffen, das Leben wird verringert
+	public void lebenVerringern() {
+		leben--;
+	}
+	
+	// Überprüfung, ob Spieler noch Leben hat
+	public boolean lebenVorhanden() {
+		return (leben > 0) ? true : false; 
+	}
+
+	public int getLeben() {
+		return leben;
+	}
+
+	public int getMaxRunde() {
+		return maxRunde;
+	}
+	
+	/*******************************************************
+	 * METHODEN, DIE NUR NACHRICHTEN AUSGEBEN
+	********************************************************/
+	// Diese Ausgaben sind Klassenmethoden, da sie sich nicht auf ein konkretes Spiel beziehen, sondern
+	// generell für jedes Spiel gelten. Sie sind nicht abhängig von der aktuellen Runde oder restlichen Leben, die
+	// der Spieler hat.
+	// Ausnahme ist ausgebenNachrichtEnde() - hier ändert sich die Nachricht, je nachdem ob der Benutzer leben > 0 hat.
+	
+	// Ausgabe Spielbeginn
+	public static void ausgebenIntro() {
+		System.out.println("Du bist in einem Wald und hast einen Wanderstab sowie einen Rucksack voll mit Eier-Tomaten-Gurken-Sandwiches. \nEs ist sehr dunkel und du kannst kaum die Hand vor Deinen Augen sehen, als Dir plötzlich etwas begegnet. \nDu hast keine Ahnung, wer oder was es ist. Möchtest du es:\n\n1) Streicheln?\n2) Füttern?\n3) Mit dem Stock hauen?");
+	}
+	
+	// Ausgabe jede neue Runde
+	public static void ausgebenFolgebegegnung() {
+		System.out.println("\nDu setzt Deinen Weg fort. Aber - was ist das? Eine weitere Begegnung. Du entschließt sich die Begegnung: "
+				+ "\n1) zu streicheln?\n2) zu füttern?\n3) mit dem Stock zu hauen?");
+	}
+
+	// Ausgabe Spielende ob gewonnen oder verloren
+	public void ausgebenNachrichtEnde() {
+		if (leben == 0)
+			System.out.println("\n\nDu hast es leider nicht aus dem Wald geschafft. Viel Glück beim nächsten Mal.");
+		else
+			System.out.println("\n\nDer Wald lichtet sich. \nFroh am leben zu sein, setzt Du Deinen Weg fort.");
+	}
+	
+	// Ausgabe nach fehlerhafter Eingabe
+	public static void ausgebenNachrichtEingabefehler() {
+		System.out.print("\nEingabe ungültig. Möchtest Du die Begegnung: \n"
+				+ "\n1) zu streicheln?\n2) zu füttern?\n3) mit dem Stock zu hauen?");
+	}
+	
+	// Macht Zeilenumbrüche, dass es nach Neuer Seite aussieht
+	public static void leerenKonsole() {
+		for(int i=1; i<10;i++)				// Screen durch 10 Zeilenumbrüche "leeren"
+			System.out.println("\n");
+	}
+}
